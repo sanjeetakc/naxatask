481e320a3dc91ff6951c145cf2199cf540ebd5a0
sanaya123 <sanjeetakhatri7965@gmail.com>
2020-07-11 15:29:27 +0545

    second update

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,108 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 //import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:trial/JobsListView.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+      theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      //home: MyHomePage(title: 'Flutter Demo Home Page'),
-      home: JobsListView(),
+      home: new JobsListView(title: 'Details'),
     );
   }
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
-  }
-//   _MyAppState createState() => _MyAppState();
-// }
-
-// class _MyAppState extends State<MyApp> {
-//  // GoogleMapController mapController;
-//   //for List View
-//   List list = List();
-//   @override
-//   void initState() {
-//     // TODO: implement initState
-//     super.initState();
-//     //initalize list
-//     list.add("nr.1");
-//     list.add("nr.2");
-//     list.add("nr.3");
-//     list.add("nr.4");
-//     list.add("nr.5");
-//     list.add("nr.6");
-//   }
-
-//   /*//final LatLng _center = const LatLng(27.6477079, 85.3446311);
-
-//   void _onMapCreated(GoogleMapController controller) {
-//     mapController = controller;
-//   }
-// */
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-
-//           title: Text('Naxa Map Demo'),
-//           backgroundColor: Colors.brown[700],
-//         ),
-//      body: Center(
-//        child: JobsListView()
-//      ),
-//     /* GoogleMap(
-//           onMapCreated: _onMapCreated,
-//           initialCameraPosition: CameraPosition(
-//             target: _center,
-//             zoom: 11.0,
-//           ),
-//         ),*/
-//       ),
-//     );
-//   }
+  State<StatefulWidget> createState() => throw UnimplementedError();
 }
 
+class JobsListView extends StatefulWidget {
+  JobsListView({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _JobsListViewState createState() => new _JobsListViewState();
+}
+
+class _JobsListViewState extends State<JobsListView> {
+  Future<List<Details>> _getDetails() async {
+    var data = await http.get("https://reqres.in/api/users?page=1");
+    var jsonData = json.decode(data.body);
+    List<Details> details = [];
+    for (var n in jsonData) {
+      Details detail = Details(
+          n["id"], n["first_name"], n["second_name"], n["email"], n["avatar"]);
+      details.add(detail);
+    }
+    print(details.length);
+    return details;
+  }
+@override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: Container(
+        child: FutureBuilder(
+          future: _getDetails(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return Container(
+                child: Center(
+                  child: Text("Loading..."),
+                ),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                     leading: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          snapshot.data[index].picture
+                        ),
+                      ),
+                    title: Text(snapshot.data[index].firstname),
+
+                  );
+                },
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+class DetailPage extends StatelessWidget {
+
+  final Details details;
+
+  DetailPage(this.details);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(details.firstname),
+        )
+    );
+  }
+}
+
+class Details {
+  final int id;
+  final String email;
+  final String firstname;
+  final String secondname;
+  final String avatar;
+
+  Details(this.id,  this.email,this.firstname, this.secondname, this.avatar);
+}
